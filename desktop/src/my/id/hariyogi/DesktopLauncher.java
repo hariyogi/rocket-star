b60cee9371f354b82b3ce4d0bfe9787675da0eeb
hariyogi <hariyogi.office@gmail.com>
2023-04-06 15:20:50 +0800

    Init

---
+++ desktop/src/my/id/hariyogi/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package my.id.hariyogi;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import my.id.hariyogi.RocketStar;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("rocket-star");
+		new Lwjgl3Application(new RocketStar(), config);
+	}
+}
