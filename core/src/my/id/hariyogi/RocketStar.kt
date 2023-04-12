3ccece9ebe81e17bdaa9bd35b9bfbc4c4f258fc2
hariyogi <hariyogi.office@gmail.com>
2023-04-12 22:00:13 +0800

    Change meteor to star

--- core/src/my/id/hariyogi/RocketStar.kt
+++ core/src/my/id/hariyogi/RocketStar.kt
@@ -3,14 +3,13 @@ package my.id.hariyogi
 import com.badlogic.gdx.Gdx
 import my.id.hariyogi.base.AnimationUtils
 import my.id.hariyogi.base.BaseActor
-import my.id.hariyogi.entity.Explosion
-import my.id.hariyogi.entity.Meteor
+import my.id.hariyogi.entity.Star
 import my.id.hariyogi.entity.Rocket
 
 class RocketStar: GameBeta() {
 
     private lateinit var rocket: BaseActor
-    private lateinit var meteor: BaseActor
+    private lateinit var star: BaseActor
     private lateinit var background: BaseActor
     private lateinit var explosion: BaseActor
 
@@ -23,17 +22,13 @@ class RocketStar: GameBeta() {
         rocket = Rocket(20f, 20f)
         mainStage.addActor(rocket)
 
-        meteor = Meteor(380f, 380f)
-        mainStage.addActor(meteor)
-
-        explosion = Explosion(80f, 80f)
-        mainStage.addActor(explosion)
-
+        star = Star(380f, 380f)
+        mainStage.addActor(star)
     }
 
     override fun update(deltaTime: Float) {
-        if(rocket.isOverlaps(meteor)) {
-            meteor.remove()
+        if(rocket.isOverlaps(star)) {
+            star.remove()
         }
     }
 }
--- core/src/my/id/hariyogi/entity/Star.kt
+++ core/src/my/id/hariyogi/entity/Star.kt
@@ -1,14 +1,13 @@
 package my.id.hariyogi.entity
 
-import com.badlogic.gdx.scenes.scene2d.Action
 import com.badlogic.gdx.scenes.scene2d.actions.Actions
 import my.id.hariyogi.base.AnimationUtils
 import my.id.hariyogi.base.BaseActor
 
-class Meteor(x: Float, y: Float): BaseActor(x, y) {
+class Star(x: Float, y: Float): BaseActor(x, y) {
 
     init {
-        val texturePath = "meteor.png"
+        val texturePath = "star.png"
         initTexture(AnimationUtils.loadSingleTexture(texturePath))
 
         setSizeAndOrigin(60f, 60f)
